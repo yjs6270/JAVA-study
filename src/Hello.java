331ef49c9b959795bfc7680a19b5195af097d24c
jiseok <didwltjr00@naver.com>
2022-04-19 16:33:37 +0900

    commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Hello {
+//백준 단계별 문제 1번
+//public class Hello {
+//    public static void main(String[] args) {
+//        System.out.println("강한친구 대한육군");
+//        System.out.println("강한친구 대한육군");
+//    }
+//}
+//백준 단계별 문제 2번
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
 }
