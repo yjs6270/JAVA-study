26bc0f5e2680ccb474cffc5f8f03eebb40382b6c
jiseok <didwltjr00@naver.com>
2022-05-06 23:21:33 +0900

    commit13

---
+++ src/baekjoon25083.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class baekjoon25083 {
+    public static void main(String[] args) {
+        System.out.printf("         ,%c\'\"%d\n",'r',7); // (a)
+        System.out.printf("%c`-_   ,\'  ,/\n",'r');
+        System.out.printf(" \\. \". %c_%c\'\n",'L','r');
+        System.out.printf("   `~\\/\n");
+        System.out.printf("      |\n");
+        System.out.printf("      |\n");
+    }
+}
